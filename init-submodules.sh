4cf25525d2960f406cc3fd0b79ecae5fd6dcba49
Vlad Vergu <v.vergu@gmail.com>
2014-03-05 11:42:03 +0100

    Submodules init

---
+++ init-submodules.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+## Set all repos
+
+git submodule add git://github.com/metaborg/mb-rep.git mb-rep &&
+git submodule add git://github.com/metaborg/mb-exec.git mb-exec &&
+git submodule add git://github.com/metaborg/mb-exec-deps.git mb-exec-deps &&
+git submodule add git://github.com/metaborg/jsglr.git jsglr &&
+git submodule add git://github.com/metaborg/runtime-libraries.git runtime-libraries &&
+git submodule add git://github.com/metaborg/spoofax.git spoofax &&
+git submodule add git://github.com/metaborg/spoofax-debug.git spoofax-debug &&
+git submodule add git://github.com/metaborg/spoofax-deploy.git spoofax-deploy &&
+git submodule add git://github.com/metaborg/sdf.git sdf &&
+git submodule add git://github.com/metaborg/rtg.git rtg &&
+git submodule add git://github.com/metaborg/box.git box &&
+git submodule add git://github.com/metaborg/esv.git esv &&
+git submodule add git://github.com/metaborg/stratego.git stratego &&
+git submodule add git://github.com/metaborg/aster.git aster &&
+git submodule add git://github.com/metaborg/nabl.git nabl &&
+git submodule add git://github.com/metaborg/ts.git ts &&
+git submodule add git://github.com/metaborg/spt.git spt &&
+git submodule add git://github.com/metaborg/tdl.git tdl &&
+git submodule add git://github.com/metaborg/imp-patched.git imp-patched &&
+git submodule add git://github.com/metaborg/java-front.git java-front &&
+git submodule add git://github.com/metaborg/strategoxt.git strategoxt &&
+git submodule add git://github.com/metaborg/strategoxt.git strategoxt-cver
+
