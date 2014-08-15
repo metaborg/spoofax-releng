4d44ae3e0ebdcde6e82fb894e57af721e2f18bdd
Gabriel Konat <gabrielkonat@gmail.com>
2014-08-15 13:08:03 -0700

    Show build products in build-all script.

--- build-all.sh
+++ build-all.sh
@@ -3,8 +3,26 @@
 set -e
 set -u
 
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+QUALIFIER=$(./latest-timestamp.sh)
+
+
+echo "Using qualifier $QUALIFIER"
+
+
 ./spoofax-deploy/org.metaborg.maven.build.strategoxt/build.sh
-./spoofax-deploy/org.metaborg.maven.build.java/build.sh
-./spoofax-deploy/org.metaborg.maven.build.spoofax.eclipse/build.sh
+./spoofax-deploy/org.metaborg.maven.build.java/build.sh -q $QUALIFIER
+./spoofax-deploy/org.metaborg.maven.build.spoofax.eclipse/build.sh -q $QUALIFIER
 ./spoofax-deploy/org.metaborg.maven.build.spoofax.libs/build.sh
 ./spoofax-deploy/org.metaborg.maven.build.spoofax.sunshine/build.sh
+
+
+ECLIPSE_UPDATE_SITE="$DIR/spoofax-deploy/org.strategoxt.imp.updatesite/target/site"
+SUNSHINE_JAR_ARRAY=("$DIR/spoofax-sunshine/org.spoofax.sunshine/target/org.metaborg.sunshine-"*".jar")
+LIBRARIES_JAR_ARRAY=("$DIR/spoofax-deploy/org.metaborg.maven.build.spoofax.libs/target/org.metaborg.maven.build.spoofax.libs-"*".jar")
+
+echo "Build products"
+echo "Eclipse update site: $ECLIPSE_UPDATE_SITE"
+echo "Sunshine JAR: ${SUNSHINE_JAR_ARRAY[0]}"
+echo "Libraries JAR: ${LIBRARIES_JAR_ARRAY[0]}"
