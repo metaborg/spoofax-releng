9a51729e2864ca69f51e75e637c3fa2d56801d20
Gabriel Konat <gabrielkonat@gmail.com>
2014-09-23 18:30:23 +0200

    Clean metaborg artifacts from local repository before doing a full build. Don't update snapshots from remotes in full build. Update submodule revisions

--- build-all.sh
+++ build-all.sh
@@ -4,19 +4,31 @@ set -e
 set -u
 
 
+# Set build vars
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 QUALIFIER=$(./latest-timestamp.sh)
+MAVEN_ARGS='-a "-nsu"'
 
 
+# Clean up local repository.
+rm -rf ~/.m2/repository/org/metaborg
+rm -rf ~/.m2/repository/.cache/tycho/org.spoofax*
+rm -rf ~/.m2/repository/.cache/tycho/org.strategoxt*
+rm -rf ~/.m2/repository/.cache/tycho/org.metaborg*
+
+
+# Run the Maven builds.
 echo "Using qualifier $QUALIFIER"
 
-./spoofax-deploy/org.metaborg.maven.build.strategoxt/build.sh
-./spoofax-deploy/org.metaborg.maven.build.java/build.sh -q $QUALIFIER
-./spoofax-deploy/org.metaborg.maven.build.spoofax.eclipse/build.sh -q $QUALIFIER
-./spoofax-deploy/org.metaborg.maven.build.spoofax.libs/build.sh
-./spoofax-deploy/org.metaborg.maven.build.spoofax.sunshine/build.sh
-./spoofax-deploy/org.metaborg.maven.build.spoofax.benchmark/build.sh
+./spoofax-deploy/org.metaborg.maven.build.strategoxt/build.sh $MAVEN_ARGS
+./spoofax-deploy/org.metaborg.maven.build.java/build.sh -q $QUALIFIER $MAVEN_ARGS
+./spoofax-deploy/org.metaborg.maven.build.spoofax.eclipse/build.sh -q $QUALIFIER $MAVEN_ARGS
+./spoofax-deploy/org.metaborg.maven.build.spoofax.libs/build.sh $MAVEN_ARGS
+./spoofax-deploy/org.metaborg.maven.build.spoofax.sunshine/build.sh $MAVEN_ARGS
+./spoofax-deploy/org.metaborg.maven.build.spoofax.benchmark/build.sh $MAVEN_ARGS
 
+
+# Echo locations of build products.
 ECLIPSE_UPDATE_SITE="$DIR/spoofax-deploy/org.strategoxt.imp.updatesite/target/site"
 SUNSHINE_JAR_ARRAY=("$DIR/spoofax-sunshine/org.spoofax.sunshine/target/org.metaborg.sunshine-"*"-shaded.jar")
 BENCHMARK_JAR_ARRAY=("$DIR/spoofax-benchmark/org.metaborg.spoofax.benchmark.cmd/target/org.metaborg.spoofax.benchmark.cmd-"*".jar")
